e39e4b4dd53a5e2d45e6fc9e1c04804675d14445
SinaTaavon <32191839+SinaTaavon@users.noreply.github.com>
2019-11-01 10:02:37 +0000

    s

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,27 @@
 
 var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function($scope, $http) {
 
-  
-  $scope.fullName = function(obj) {
-    $scope.records = obj;
+ 
+$http({
+  method: 'GET',
+  url: 'https://raw.githubusercontent.com/vega/vega/master/docs/data/movies.json'
+}).then(function successCallback(response) {
+  if(response && response.data) {
+    $scope.records = response.data;
     console.log($scope.records);
-  };
+  }
+  else{
+  $scope.records = []
+  }
 
-    var HttpClient = function() {
-      this.get = function(aUrl, aCallback) {
-          var anHttpRequest = new XMLHttpRequest();
-          anHttpRequest.onreadystatechange = function() { 
-              if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                  aCallback(anHttpRequest.responseText);
-          }
-    
-          anHttpRequest.open( "GET", aUrl, true );            
-          anHttpRequest.send( null );
-      }
-    }
-    
-    var client = new HttpClient();
-    client.get('https://raw.githubusercontent.com/vega/vega/master/docs/data/movies.json', function(response) {
-    
-      var parsedData = JSON.parse(response);
-    
-      for(var i = 0; i < parsedData.length; i++) {
-        var obj = parsedData[i];
+  }, function errorCallback(response) {
+    $scope.records = []
+  });
 
-        $scope.fullName(obj);   
-    }
-        // do something with response
-    });
-    
-    
+  $scope.orderByMe = function(x) {
+    $scope.myOrderBy = x;
+}
+
+
 });
