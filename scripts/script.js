dab5b93b8990f47fb01094c3f8b189d3910d1375
SinaTaavon <32191839+SinaTaavon@users.noreply.github.com>
2019-11-01 16:08:44 +0000

    z

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ $http({
   url: 'https://raw.githubusercontent.com/vega/vega/master/docs/data/movies.json'
 }).then(function successCallback(response) {
   if(response && response.data) {
-    $scope.jsonresults = response.data;
+    $scope.imdbrev = response.data.IMDB_Rating;
     $scope.records = response.data;
     console.log($scope.records);
   }
@@ -23,6 +23,15 @@ $http({
   $scope.orderByMe = function(record) {
     $scope.myOrderBy = record;
 }
+
+$scope.whatClassIsIt= function(){
+  if($scope.imdbrev <= 5)
+         return "badge-danger"
+  else ($scope.imdbrev > 5)
+      return "badge-success";
+  
+ }
+
 
 
 });
